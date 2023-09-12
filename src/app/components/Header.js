152d2c926945bcba2a80877166fbbac9d53041ac
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-12 18:30:13 +0600

    add res navbar

--- src/app/components/Header.js
+++ src/app/components/Header.js
@@ -4,6 +4,9 @@ import Image from "next/image";
 import { Link } from "react-scroll";
 import SearchMobile from "./SearchMobile";
 import { useMediaQuery } from "react-responsive";
+// icons
+
+import { BiMenuAltRight, BiX } from "react-icons/bi";
 
 const Header = () => {
   // Initialize state variables separately
@@ -36,7 +39,108 @@ const Header = () => {
         header ? "bg-white shadow-md py-2" : "bg-transparent shadow-none py-4"
       } fixed w-full max-w-[1920px] mx-auto z-20 transition-all duration-300`}
     >
-      Header
+      <div className="xl:container mx-auto flex flex-col xl:flex-row xl:items-center xl:justify-between">
+        <div className="flex justify-between items-center px-4">
+          {/* {logo} */}
+          <Link
+            to="home"
+            smooth={desktopMode}
+            spy={true}
+            className="cursor-pointer"
+          >
+            <Image
+              src={"/icons/logo.svg"}
+              width={194}
+              height={64}
+              alt="logo"
+            ></Image>
+          </Link>
+
+          {/* Nav Open Menu */}
+          <div
+            onClick={() => setNav(!nav)}
+            className="cursor-pointer xl:hidden"
+          >
+            {nav ? (
+              <BiX className="text-4xl" />
+            ) : (
+              <BiMenuAltRight className="text-4xl"></BiMenuAltRight>
+            )}
+          </div>
+        </div>
+        {/* Nav */}
+        <nav
+          className={`${
+            nav ? "max-h-max py-4 px-4 xl:py-0" : "max-h-0 xl:max-h-max"
+          } flex flex-col w-full bg-white gap-y-6 overflow-hidden font-bold xl:font-medium xl:flex-row xl:w-max xl:gap-x-8 xl:h-max xl:bg-transparent xl:pb-0 transition-all duration-400 text-center xl:text-left uppercase text-sm xl:text-[15px] xl:normal-case`}
+        >
+          <Link
+            className="cursor-pointer"
+            to="home"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Home
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="car"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Car
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="about"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            About
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="why"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Why Us
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="testimonials"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Testimonials
+          </Link>
+          <Link
+            className="cursor-pointer"
+            to="contact"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            Contact
+          </Link>
+          <Link
+            className="xl:hidden btn btn-primary btn-sm max-w-[164px] mx-auto"
+            to="/"
+            activeClass="active"
+            smooth={desktopMode}
+            spy={true}
+          >
+            See All Cars
+          </Link>
+
+          <SearchMobile></SearchMobile>
+        </nav>
+      </div>
     </header>
   );
 };
