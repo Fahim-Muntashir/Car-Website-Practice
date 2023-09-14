ad660b6dde0f7451bdad1d87a3e536e3923264e6
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-14 12:18:33 +0600

    menu btn added

--- src/app/components/LocationSelection.js
+++ src/app/components/LocationSelection.js
@@ -1,5 +1,33 @@
+"use client";
+
+import React, { useState } from "react";
+
+// headless ui
+import { Menu } from "@headlessui/react";
+
+// icons
+import { FaMapMarkerAlt } from "react-icons/fa";
+
+// location data
+const locations = [
+  "Main Street 123, United States",
+  "Business Avenue 456,Canada",
+  "Park Road 789,United Kingdom",
+];
+
 const LocationSelection = () => {
-  return <div>Location</div>;
+  const [location, setLocation] = useState("Select Location");
+
+  return (
+    <Menu as="div" className="w-full h-full flex xl:fkex-row">
+      <div className="relative flex-1">
+        {/*  btn */}
+        <Menu.Button>Menu Btn</Menu.Button>
+        {/* Items */}
+        <Menu.Items>Menu Items</Menu.Items>
+      </div>
+    </Menu>
+  );
 };
 
 export default LocationSelection;
--- src/app/components/SearchMobile.js
+++ src/app/components/SearchMobile.js
@@ -1,5 +1,29 @@
+// components
+import DateSelection from "./DateSelection";
+import HourSelection from "./DateSelection";
+import LocationSelection from "./LocationSelection";
+
 const SearchMobile = () => {
-  return <div>Nobike Seraic</div>;
+  return (
+    <div className="xl:hidden font-medium">
+      <div className="container mx-auto">
+        <div className=" flex flex-col gap-y-4">
+          {/* location selection */}
+          <LocationSelection></LocationSelection>
+          {/* Data Selection */}
+          <DateSelection></DateSelection>
+          {/* Hourse Selection */}
+          <HourSelection></HourSelection>
+          {/* Btn */}
+          <div className="flex items-center px-6">
+            <button className="btn btn-sm btn-accent w-[164px] mx-auto ">
+              Search
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default SearchMobile;
