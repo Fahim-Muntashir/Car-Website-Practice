90097c8e5e2f853438a6dd670dd18ce41c312767
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-16 21:45:30 +0600

    added comnit

--- src/app/components/Cta.js
+++ src/app/components/Cta.js
@@ -1,10 +1,42 @@
+"use client";
+
+// next image
+import Image from "next/image";
+
+// framer-motion
+import { motion } from "framer-motion";
+
+//variants
+import { fadeIn } from "/variants";
+
 const Cta = () => {
   return (
     <section
       className="pt-24 xl:pt-48 flex items-center pb-0 bg-[#b3b72c]/10"
       id="contact"
     >
-      Cta
+      <div>
+        {/* text */}
+
+        <div></div>
+
+        {/* image */}
+        <motion.div
+          variants={fadeIn("up", 0.8)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.2 }}
+          className="flex-1 flex justify-center order-1
+        md:order-none "
+        >
+          <Image
+            src={"/images/cta/phone.svg"}
+            width={320}
+            height={640}
+            alt=""
+          />
+        </motion.div>
+      </div>
     </section>
   );
 };
--- src/app/components/TestimonialSlider.js
+++ src/app/components/TestimonialSlider.js
@@ -49,15 +49,30 @@ const testimonialData = [
 
 const TestimonialSlider = () => {
   return (
-    <div>
-      <Swiper>
+    <motion.div
+      variants={fadeIn("up", 0.4)}
+      initial="hidden"
+      whileInView={"show"}
+      viewport={{ once: false, amount: 0.6 }}
+      className="container mx-auto"
+    >
+      <Swiper
+        pagination={{
+          clickable: true,
+          dynamicBullets: true,
+        }}
+        modules={[Pagination]}
+        className="h-[450px] xl:h-[400px]"
+      >
         {testimonialData.map((person, index) => {
           const { message, avatar, name, job } = person;
           return (
             <SwiperSlide key={index}>
-              <div>
+              <div className="flex flex-col justify-center items-center text-center">
                 <FaQuoteLeft className="text-7xl text-accent mb-6" />
-                <div>{message}</div>
+                <div className="text-2xl xl:text-4xl max-w-[874px] mb-12 font-medium">
+                  {message}
+                </div>
                 <Image
                   className="mb-4"
                   src={avatar}
@@ -65,12 +80,14 @@ const TestimonialSlider = () => {
                   height={64}
                   alt=""
                 ></Image>
+                <div className="text-lg font-medium">{name}</div>
+                <div className="text-secondary">{job}</div>
               </div>
             </SwiperSlide>
           );
         })}
       </Swiper>
-    </div>
+    </motion.div>
   );
 };
 
