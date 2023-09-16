785811f20edaa8b43247f00d486b2d5ffa3c2c32
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-16 15:59:03 +0600

    added import for why

--- src/app/components/Why.js
+++ src/app/components/Why.js
@@ -1,3 +1,17 @@
+"use client";
+
+// next image
+import Image from "next/js";
+
+// framer motion
+import { motion } from "framer-motion";
+
+// variants
+import { fadeIn } from "/variants";
+
+// icons
+import { MdHandshake, MdKey, MdTrendingUp } from "react-icons/md";
+
 const Why = () => {
   return (
     <section
