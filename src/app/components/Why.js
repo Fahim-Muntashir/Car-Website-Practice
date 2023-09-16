8aae98ef53c98f9a88ade491cf85fe67c511bfb2
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-16 16:18:52 +0600

    about added without frid items

--- src/app/components/Why.js
+++ src/app/components/Why.js
@@ -1,7 +1,7 @@
 "use client";
 
 // next image
-import Image from "next/js";
+import Image from "next/image";
 
 // framer motion
 import { motion } from "framer-motion";
@@ -14,10 +14,41 @@ import { MdHandshake, MdKey, MdTrendingUp } from "react-icons/md";
 
 const Why = () => {
   return (
-    <section
-      className="section flex items-center bg-green-200"
-      id="why"
-    ></section>
+    <section className="section flex items-center" id="why">
+      <div className="container mx-auto">
+        <motion.h2
+          variants={fadeIn("up", 0.2)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="h2 text-center"
+        >
+          Unmatched excellence and customer satisfaction
+        </motion.h2>
+        <motion.p
+          variants={fadeIn("up", 0.4)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="max-w-[680px] text-center mx-auto mb-2"
+        >
+          Our desication to providing excentional services sets us apart from
+          the competition. From the moment you engage with us, we strive to
+          exceed your expectations in every interaction
+        </motion.p>
+        {/* Car Image */}
+        <motion.div
+          variants={fadeIn("up", 0.6)}
+          initial="hidden"
+          whileInView={"show"}
+          viewport={{ once: false, amount: 0.6 }}
+          className="hidden md:flex justify-center mb-6 xl:mb-2"
+        >
+          <Image src={"/images/why/car.svg"} width={1060} height={420} alt="" />
+        </motion.div>
+        {/* grid items */}
+      </div>
+    </section>
   );
 };
 
