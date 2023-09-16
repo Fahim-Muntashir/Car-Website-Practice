78203350c8de1291c3ab34af622f8f708720c883
FahimMuntashir1 <fahimasdf07@gmail.com>
2023-09-16 09:09:13 +0600

    some varients added

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -7,6 +7,9 @@ import Search from "./Search";
 // comtext
 import { SearchContext } from "../context/search";
 
+// image next
+import Image from "next/image";
+
 const Hero = () => {
   const { searchActive } = useContext(SearchContext);
 
@@ -14,6 +17,51 @@ const Hero = () => {
     <section className="h-screen xl:h-[90vh] bg-[#b2b7c2]/10 " id="home">
       <div className="container mx-auto h-full xl:pt-10">
         {/* text and img wrapper */}
+        <div className="flex flex-col xl:flex-row justify-center items-center xl:justify-start h-full">
+          {/* text */}
+          <div className="text-center xl:max-w-xl xl:text-left mt-16 xl:mt-0">
+            <h1 className="h1">
+              Explore the Finest <span className="text-accent">Global</span>
+              Offers
+            </h1>
+            <p className="description max-w-[550px] mx-auto xl:mx-0 mb-6 xl:mb-10">
+              Find your ideal roide for any adventure with our diverse range of
+              affordable and dependable car rentals.
+            </p>
+
+            {/* btns */}
+            <div className="flex gap-x-3 justify-center xl:justify-start">
+              {/* btn app store */}
+              <button className="btn-cta">
+                <Image
+                  src={"/icons/buttons/app-store.svg"}
+                  width={132}
+                  height={36}
+                  alt=""
+                ></Image>
+              </button>
+              {/* btn google */}
+              <button className="btn-cta">
+                <Image
+                  src={"/icons/buttons/google-play.svg"}
+                  width={132}
+                  height={36}
+                  alt=""
+                ></Image>
+              </button>
+            </div>
+          </div>
+          {/* img  car*/}
+          <div className="relative w-full h-full max-h-[50vh] md:max-w-[70vw] xl:max-w-[860px] xl:max-h-[542px] xl:absolute xl:right-0 xl:top-48">
+            <Image
+              src={"/images/hero/car.svg"}
+              fill
+              alt=""
+              style={{ objectFit: "contain" }}
+              priority
+            ></Image>
+          </div>
+        </div>
       </div>
 
       {searchActive ? (
